e9fa585b3f623f119d826ecd7d5ac83b19378c8d
mohd sahib <133562502+mdSahibMalik@users.noreply.github.com>
2026-01-19 01:28:51 +0530

    Update cookie settings for secure transmission

--- server/src/utils/sendToken.js
+++ server/src/utils/sendToken.js
@@ -14,8 +14,10 @@ export const sendToken = async (user, res) => {
         Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      secure: false, // ✅ set to true ONLY in production with HTTPS
-      sameSite: "Lax", // ✅ prevent CSRF in most cases
+      // secure: false, // ✅ set to true ONLY in production with HTTPS
+      // sameSite: "Lax", // ✅ prevent CSRF in most case
+  secure: true,          // REQUIRED on HTTPS
+  sameSite: "none",      // REQUIRED for cross-site
     })
     .json({
       token,
